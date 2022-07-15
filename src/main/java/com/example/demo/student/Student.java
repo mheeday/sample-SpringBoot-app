931243610ad94c0d00bce2c549c92f7dd0d71650
mheeday <66279843+mheeday@users.noreply.github.com>
2022-07-15 11:46:48 +0100

    first commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,136 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+
+@Entity
+@Table
+public class Student {
+
+
+    @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    private String email;
+
+    @Transient
+    private Integer age;
+
+    public Student() {
+    }
+
+
+    public Student(String name, 
+                    LocalDate dob, 
+                    String email) {
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+    }
+    
+
+    public Student(Long id, String name, LocalDate dob, String email) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return this.dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Student id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public Student name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public Student dob(LocalDate dob) {
+        setDob(dob);
+        return this;
+    }
+
+    public Student email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    public Student age(Integer age) {
+        setAge(age);
+        return this;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", dob='" + getDob() + "'" +
+            ", email='" + getEmail() + "'" +
+            ", age='" + getAge() + "'" +
+            "}";
+    }
+
+
+}
